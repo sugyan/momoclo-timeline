57637b8b5d21f69dfeca597c7f4e25981a0ef703
sugyan <sugi1982@gmail.com>
2012-05-16 18:59:50 +0900

    update views

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,8 +1,8 @@
 $(function () {
     var timeline = new VMM.Timeline();
     var config = {};
-    var current = Number(window.location.hash.substring(1));
-    if (typeof current === 'number') {
+    var current = parseInt(window.location.hash.substring(1));
+    if (typeof current === 'number' && ! isNaN(current)) {
         config.current_slide = current;
     } else {
         config.start_at_end = true;
