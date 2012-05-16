a383231fd200bb4591f6079177c45bb78c16ed8d
sugyan <sugi1982@gmail.com>
2012-05-16 21:30:51 +0900

    episode edit

--- lib/routes/episode.rb
+++ lib/routes/episode.rb
@@ -2,17 +2,24 @@ require 'sinatra/base'
 require 'haml'
 
 class App < Sinatra::Base
+
+  def process_date(p)
+    if datetype = p.delete('datetype')
+      p['startdate'] = p.delete('startdate' + datetype)
+      if datetype == '1'
+        p['enddate'] = (Date.parse(p['startdate']) >> 1) - 1
+      else
+        p['enddate'] = nil
+      end
+    end
+  end
+
   get '/episode/new' do
     haml :'episode/new', :locals => { :params => {}, :errors => {} }
   end
 
   post '/episode/new' do
-    if datetype = params.delete('datetype')
-      params['startdate'] = params.delete('startdate' + datetype)
-      if datetype == '1'
-        params['enddate'] = (Date.parse(params['startdate']) >> 1) - 1
-      end
-    end
+    process_date(params)
     logger.info params
     episode = Episode.new(params)
     if episode.valid?
@@ -29,6 +36,46 @@ class App < Sinatra::Base
   get '/episode/:id' do
     if e = Episode[params[:id]]
       haml :'episode/index', :locals => { :episode => e }
+    else
+      not_found 'Not Found'
+    end
+  end
+
+  get '/episode/:id/edit' do
+    if (e = Episode[params[:id]]) && session[:user] && session[:user][:id] == e.user_id
+      params = {
+        'type'     => e.type.to_s,
+        'text'     => e.text,
+        'datetype' => e.enddate ? '1' : '0',
+      }
+      params["startdate#{ params['datetype'] }"] = e.startdate.to_s
+      haml :'episode/new', :locals => {
+        :params => params,
+        :errors => {},
+      }
+    else
+      not_found 'Not Found'
+    end
+  end
+
+  post '/episode/:id/edit' do
+    if e = Episode[params[:id]]
+      if session[:user] && session[:user][:id] == e.user_id
+        process_date(params)
+        logger.info params
+        e.set_fields(params, ['type', 'startdate', 'enddate', 'text'])
+        if e.valid?
+          e.update_only(params)
+          redirect "/episode/#{ e.id }"
+        else
+          haml :'episode/new', :locals => {
+            :params => request.params,
+            :errors => e.errors,
+          }
+        end
+      else
+        error 400, 'Bad Request'
+      end
     else
       not_found 'Not Found'
     end
